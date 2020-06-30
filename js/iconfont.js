c88ec9d246f05e82b562ab945f6e9f7cb97f91ee
xiabo2 <10294370@qq.com>
2020-07-01 00:27:15 +0800

    Site updated: 2020-07-01 00:27:07

---
+++ js/iconfont.js
@@ -0,0 +1,3 @@
+$(".card-info-social-icons").append(
+    '<a class="social-icon" href="https://www.baidu.com" target="_blank"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-qq1"></use></svg></a>'
+);
